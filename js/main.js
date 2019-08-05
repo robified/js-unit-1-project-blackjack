246af902ac4b0b8524471a27184270df36553cfe
Robin <daorob@gmail.com>
2019-08-05 10:48:39 -0500

    Add function skeletons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 /*----- constants -----*/ 
+let player = {};
+let dealer = {};
 
 
 
@@ -15,4 +17,28 @@
 
 
 /*----- functions -----*/
+// to kick off the game
+function init() {
 
+}
+
+// might need more than one render fn, not sure yet
+function render() {
+
+}
+
+// "Press the start button to start."
+    start();
+    deal();
+    
+// For player 1 to receive a random card
+    hit();
+
+// To calculate player 1's points
+    calculate();
+
+// For player 1 to stop receiving a card
+    stand();
+
+// For player 1 to restart
+    replay();
