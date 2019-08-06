c9a608bd2efae9a2c51a83b0ca6b2f00e6b3fe1e
Robin <daorob@gmail.com>
2019-08-06 14:04:35 -0500

    Create a deck of 52 cards

--- js/main.js
+++ js/main.js
@@ -1,22 +1,57 @@
 /*----- constants -----*/ 
-let player = {};
-let dealer = {};
+const deck = makeADeck();
 
+
+
+
+
+
+// class Players {
+
+// }
+
+// const p1 = {
+//     score: 0,
+//     cardInHand: []
+// };
+
+// const dealer = {
+//     score: 0,
+//     cardInHand: []
+// };
 
 
 /*----- app's state (variables) -----*/ 
 
 
-
 /*----- cached element references -----*/ 
-
+ let butDeal = document.getElementById('deal');
+ let butHit = document.getElementById('hit');
+ let butStand = document.getElementById('stand');
+ let butReplay = document.getElementById('replay');
 
 
 /*----- event listeners -----*/ 
 
 
-
 /*----- functions -----*/
+function makeADeck() { // to make the deck
+    let deck = [];
+    let suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    let values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+    suits.forEach(function(suit){
+        values.forEach(function(value){
+            deck.push({suit: `${suit}`, value: `${value}`, cardName: `${value} of ${suit}`, cardImage: `${suit}${value}.jpg`});
+        })
+    }); 
+    return deck;
+};
+
+
+
+
+
 // to kick off the game
 function init() {
 
@@ -28,17 +63,33 @@ function render() {
 }
 
 // "Press the start button to start."
-    start();
-    deal();
+    function start() {
+
+    };
+    function deal() {
+
+    };
     
 // For player 1 to receive a random card
-    hit();
+    function hit() {
+
+    };
 
 // To calculate player 1's points
-    calculate();
+    function calculate() {
+// If 
+//     value = Ace, then = 1 or 11
+//     value = Jack/Queen/King = 10
+
+    };
 
 // For player 1 to stop receiving a card
-    stand();
+    function stand() {
+
+    };
+
 
 // For player 1 to restart
-    replay();
+    function replay() {
+
+    };
